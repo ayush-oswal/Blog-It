caa4da8dab752b6984f919ece3cdf90abc7632d3
ayush-oswal <ayushoswal2003@gmail.com>
2024-01-04 11:12:23 +0530

    fix: delete post

--- server/controllers/deletePost.js
+++ server/controllers/deletePost.js
@@ -1,8 +1,17 @@
 import Post from "../models/post.js";
+import fs from 'fs';
 
 export const deletePost = async(req,res) =>{
     const id = req.params.id;
+    const {Cover: cover} = await Post.findById(id);
+    // console.log(post)
+    if (cover) {
+        const cwd = process.cwd();
+        // console.log(cover)
+        fs.rmSync(cwd + "/uploads/" + cover);
+    }
     const deletedPost = await Post.findByIdAndDelete(id);
+
     if (!deletedPost) {
         return res.status(404).json({ message: 'Post not found' });
     }
